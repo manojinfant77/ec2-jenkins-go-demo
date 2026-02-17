0d187d1dab89850d537b45110feb249afb49bc85
Manoj Infant <manojinfant77@gmail.com>
2026-02-17 18:10:05 +0530

    Added and in the sentence and aws

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from Jenkins Dynamic EC2 🚀 With CI/CD..")
+		fmt.Fprintf(w, "Hello from Jenkins and AWS Dynamic EC2 With CI/CD..")
 	})
 	http.ListenAndServe(":8080", nil)
 }
